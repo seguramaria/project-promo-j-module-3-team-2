f4d3db657aee473a873da63d8ffaa2225b9bb6f9
BbeatrizSI <bbeatriz@gmail.com>
2020-07-27 13:25:19 +0200

    run docs

---
+++ my-project/docs/precache-manifest.c7e5c11a6239051b7e91cc9bd477c8dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747123fa6da3a1e6e089fb4d9004632b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74868e00742f49d31c51",
+    "url": "./static/css/main.dbf9b9e8.chunk.css"
+  },
+  {
+    "revision": "cda94d6d344bcd4c3658",
+    "url": "./static/js/2.7ba261f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7ba261f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74868e00742f49d31c51",
+    "url": "./static/js/main.1d511a4e.chunk.js"
+  },
+  {
+    "revision": "aa31d445de8c4a269e93",
+    "url": "./static/js/runtime-main.863985a8.js"
+  },
+  {
+    "revision": "e6ef284d948d4107275581ce9cd34956",
+    "url": "./static/media/azul.e6ef284d.svg"
+  },
+  {
+    "revision": "61abc6d109702e06b5b2eb6f6fb2cade",
+    "url": "./static/media/fondo.61abc6d1.jpg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  },
+  {
+    "revision": "dbc25e7d72a8696f76a786f2695971a3",
+    "url": "./static/media/moon.dbc25e7d.svg"
+  },
+  {
+    "revision": "6a7c817a555a42a71c269fcb35460cec",
+    "url": "./static/media/sailor-noche.6a7c817a.jpg"
+  },
+  {
+    "revision": "159cead6df704ffc45af7801e1266f5f",
+    "url": "./static/media/verderosa.159cead6.svg"
+  }
+]);
